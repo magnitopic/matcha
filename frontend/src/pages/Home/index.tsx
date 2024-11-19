d9611345312879d3772002c75e72f77cd7babec2
Alejandro Magnitopic <magnitopic@gmail.com>
2024-11-19 10:18:54 +0100

    feat/initial routing and pages (#4)
    * initial frontend structure
    
    * initial routing and pages
    
    * responsive header footer & 404 page
    
    * favicon
    
    * Landing page

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainHeader from "./MainHeader";
+
+const index = () => {
+	return (
+		<main className="flex flex-1 justify-center items-center flex-col">
+			<MainHeader />
+		</main>
+	);
+};
+
+export default index;
